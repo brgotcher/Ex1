6de023c357bcc9aa3ae91b7055217f2522a7456d
Reed Gotcher <brgotcher@gmail.com>
2019-08-27 20:37:30 -0500

    more work on event listener for insert button

--- src/MeaningStatic.java
+++ src/MeaningStatic.java
@@ -1,11 +1,11 @@
-//public class MeaningStatic {
-//
-//    public static void main(String[] args) {
-//        //class member vs instance member
-//
-//    }
-//}
-//
-//class Foo {
-//    int age; //instance(object) member
-//}
+public class MeaningStatic {
+
+    public static void main(String[] args) {
+        //class member vs instance member
+
+    }
+}
+
+class Foo {
+    int age; //instance(object) member
+}
